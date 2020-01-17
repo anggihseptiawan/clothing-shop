ccb0d3a1dfaa04148180941a03c9bea4bb983a51
Anggih Septiawan <albertanggih@gmail.com>
2020-01-17 10:12:55 +0700

    Updates

---
+++ precache-manifest.2f60f84d3d866a101be9df6b6d484f02.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce98e390a9393d843417a3a284eeced8",
+    "url": "/anggihseptiawan/clothing-shop/index.html"
+  },
+  {
+    "revision": "4d2d652fb9b9efb7e0c9",
+    "url": "/anggihseptiawan/clothing-shop/static/css/main.77f3373e.chunk.css"
+  },
+  {
+    "revision": "a185adead941ed7fed05",
+    "url": "/anggihseptiawan/clothing-shop/static/js/2.57633bcd.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/anggihseptiawan/clothing-shop/static/js/2.57633bcd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d2d652fb9b9efb7e0c9",
+    "url": "/anggihseptiawan/clothing-shop/static/js/main.1539b0ea.chunk.js"
+  },
+  {
+    "revision": "39de94eb3a2e84a7bccf",
+    "url": "/anggihseptiawan/clothing-shop/static/js/runtime-main.b80656d9.js"
+  },
+  {
+    "revision": "c878facff7601f97699567d97f0226d6",
+    "url": "/anggihseptiawan/clothing-shop/static/media/crown.c878facf.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/anggihseptiawan/clothing-shop/static/media/shopping-bag.30f07071.svg"
+  }
+]);
